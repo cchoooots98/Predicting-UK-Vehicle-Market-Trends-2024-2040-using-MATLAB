57f836239450adc041b5e2aadce26f237bb3eb33
cchoooots98 <yyfeng2626@gmail.com>
2025-02-23 01:26:40 -0500

    Update the calculation method of the number of survived vehicles

--- fulfil_lowerR_all_matrix.m
+++ fulfil_lowerR_all_matrix.m
@@ -5,8 +5,7 @@ for col=predict_years_num+1:predict_years_num+years_col_num
             &&mid_rate_all(mid_rate_index)>0
         firstNonNaNIndex = find(~isnan(all_matrix(:,col)), 1, 'first');
         numNonNan=years_raw_num-firstNonNaNIndex+1;
-        all_matrix(years_raw_num+1:end,col)=...
-            sum(all_matrix(firstNonNaNIndex:years_raw_num,col)./mid_rate_all(mid_rate_index-numNonNan+1:mid_rate_index))/numNonNan...
+        all_matrix(years_raw_num+1:end,col)=all_matrix(years_raw_num,col)/mid_rate_all(mid_rate_index)...
             .*mid_rate_all(mid_rate_index+1:mid_rate_index+predict_years_num);
     else
         all_matrix(years_raw_num+1:end,col)=zeros(predict_years_num,1);
